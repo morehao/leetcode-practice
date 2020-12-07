a82219c569a94d7f4ec94012fd850413b19db201
morehao <morehao@qq.com>
2020-12-07 23:01:54 +0800

    feat(practice): 62. 不同路径

--- 62.不同路径.js
+++ 62.不同路径.js
@@ -12,7 +12,7 @@
  */
 
 // 状态转移方程：f(i, j) = f(i - 1, j) + f(i, j - 1)
-var uniquePaths = function(m, n) {
+var uniquePaths = (m, n) => {
   const initArr = len => new Array(len);
   const result = initArr(m).fill(initArr(n).fill(1));
   for (let i = 1; i < m; i++) {
@@ -20,7 +20,7 @@ var uniquePaths = function(m, n) {
       result[i][j] = result[i - 1][j] + result[i][j - 1];
     }
   }
-  return result.pop().pop();
+  return result[m - 1][n - 1];
 };
 // @lc code=end
 
